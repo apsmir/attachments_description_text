da8131834d10873cdd32525bdb8f1e35b9cad8a1
apsmir <ap.smir@gmail.com>
2025-05-15 12:29:17 +0300

    add plugin info

--- init.rb
+++ init.rb
@@ -2,8 +2,8 @@ Redmine::Plugin.register :attachments_description_text do
   name 'Redmine attachments description to text'
   author 'Alexey Smirnov'
   description 'This is a plugin for Redmine, change attachments description type from string to text'
-  version '0.0.1'
-  #url 'https://github.com/apsmir/redmine_extend'
+  version '1.0'
+  url 'https://github.com/apsmir/attachments_description_text'
   author_url 'https://github.com/apsmir'
 
 end
